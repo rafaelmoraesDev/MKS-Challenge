706050dea8830d3d9536c271d5ca2831588f5446
RafaRM <rafaelmoraes.analyst@gmail.com>
2023-02-12 14:58:43 -0300

    Start project

---
+++ Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public float Velocity = 0f;
+    void Update()
+    {
+        float axysY = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(0f, axysY, 0f);
+
+        transform.Translate(direction * Velocity * Time.deltaTime);
+        //transform.RotateAround();
+    }
+}
