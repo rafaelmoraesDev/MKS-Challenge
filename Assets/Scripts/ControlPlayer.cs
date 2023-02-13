822b41d2ba8af99fed04595c7ddf2247e0663018
RafaRM <rafaelmoraes.analyst@gmail.com>
2023-02-13 18:50:53 -0300

    Set removes and modifications

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class ControlPlayer : MonoBehaviour
 {
-    public float Velocity = 0f;
-    void Update()
+
+    public GameObject CannonBall;
+    public GameObject ExitPointCannon;
+    static float ZERO = 0f;
+
+    private StatusCharacter statusCharacter;
+
+    private Rigidbody2D rgdBody2D;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    private void Awake()
+    {
+        rgdBody2D = GetComponent<Rigidbody2D>();
+        statusCharacter = GetComponent<StatusCharacter>();
+    }
+    private void LateUpdate()
+    {
+        MovePlayer();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(CannonBall, ExitPointCannon.transform.position , ExitPointCannon.transform.rotation);
+        }
+    }
+
+    private void MovePlayer()
     {
         float axysY = Input.GetAxis("Vertical");
+        float axysZ = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(ZERO, axysY, ZERO);
 
-        Vector3 direction = new Vector3(0f, axysY, 0f);
+        if (axysY > ZERO)
+            transform.Translate(direction * speed * Time.deltaTime);
 
-        transform.Translate(direction * Velocity * Time.deltaTime);
-        //transform.RotateAround();
+        if (!axysZ.Equals(0))
+            transform.Rotate(new Vector3(ZERO, ZERO, axysZ));
     }
+
+    public void SetDamage()
+    {
+        statusCharacter.Life -= statusCharacter.Damage;
+
+        if (statusCharacter.Life <= ZERO)
+        {
+            Debug.Log("Die");
+        }
+    }
+
 }
